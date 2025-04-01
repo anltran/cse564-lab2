0ba619ce7c87aa3395f2277c2f0c02bc611240e9
anltran <tran.anthony@aol.com>
2025-04-01 13:58:37 -0400

    Task 4 Preprocessing

--- data_processing.py
+++ data_processing.py
@@ -12,6 +12,8 @@ from sklearn.metrics import mean_squared_error
 
 from kneed import KneeLocator
 
+from sklearn.manifold import MDS
+
 # Numerical features in spotify dataset
 features = ['artist_count', 'in_spotify_playlists', 'streams', 'in_apple_playlists', 'in_deezer_playlists', 'bpm', 'danceability', 'valence', 'energy', 'acousticness', 'instrumentalness', 'liveness', 'speechiness']
 
@@ -31,7 +33,7 @@ df['in_deezer_playlists'] = df['in_deezer_playlists'].astype(int)
 X = StandardScaler().fit_transform(df)
 
 # Apply PCA
-pca = PCA()
+pca = PCA(random_state=42)
 pca_values = pca.fit_transform(X)
 
 eigenvalues = pca.explained_variance_
@@ -56,7 +58,7 @@ mse_scores = []
 cluster_ids = pd.DataFrame()
 
 for i in range(1,11):
-    kmeans = KMeans(n_clusters=i)
+    kmeans = KMeans(n_clusters=i, random_state=42)
     kmeans.fit(X)
 
     labels = kmeans.labels_
@@ -111,3 +113,27 @@ with open('./static/loadings.csv', 'w', newline='') as f:
 
 result = pd.concat([df, cluster_ids], axis=1)
 result.to_csv('./static/spotify-clustered.csv', index=False)
+
+# Run MDS on dataset and write the output and cluster ids to a CSV file
+embedding = MDS(n_components=2, dissimilarity='euclidean', random_state=42)
+mds = embedding.fit_transform(df)
+with open('./static/data_mds.csv', 'w', newline='') as f:
+    labels = ['x', 'y']
+    for i in range(10):
+        labels.append('kmeans_' + str(i+1))
+    writer = csv.writer(f)
+    writer.writerow(labels)
+    for i in range(len(mds)):
+        row = [mds[i][0], mds[i][1]]
+        for j in range(10):
+            row.append(cluster_ids['kmeans_' + str(j+1)][i])
+        writer.writerow(row)
+
+
+embedding = MDS(n_components=2, dissimilarity='precomputed', random_state=42)
+mds = embedding.fit_transform(1-abs(df.corr()))
+with open('./static/var_mds.csv', 'w', newline='') as f:
+    writer = csv.writer(f)
+    writer.writerow(['x', 'y'])
+    for x, y in mds:
+        writer.writerow([x, y])
